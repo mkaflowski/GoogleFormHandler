6c2c99d66e15410aa69f88d2d63483427424a0ff
Mateusz <mkaflowski@gmail.com>
2016-09-14 16:44:16 +0200

    first commit

---
+++ app/src/main/java/mkaflowski/googleformhandler/MainActivity.java
@@ -0,0 +1,13 @@
+package mkaflowski.googleformhandler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
